c913ed93ffdedce6c5c03f0a16a5f07535d67007
galletaadev <marcos.castillo.chap@gmail.com>
2023-09-25 08:50:10 +0200

    primeros cambios ejercicio1
    t1

---
+++ exercise1.js
@@ -0,0 +1,15 @@
+const TEXT_BLUE = "%cWelcome to the application!"
+
+const TEXT_GREEN = "%cThis is an informal message."
+
+const TEXT_WARNING = "%cThis is a warning. Be cautius."
+
+const TEXT_ERROR = "%cThis is a error! Something went wrong."
+
+console.log(TEXT_BLUE, "color:blue; font-style: bold; font-size: 18px")
+
+console.log(TEXT_GREEN, "color:green; font-size: 16px")
+
+console.warn(TEXT_WARNING, "color:orange; font-size: 16px")
+
+console.error(TEXT_ERROR, "color:red; font-size: 16px")
